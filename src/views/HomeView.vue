237c32994a9f1e6381efb2ffedd219813c49feac
Kingsley Etukudo <kingsleyj90@gmail.com>
2022-09-02 16:11:28 +0100

    okay

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -118,8 +118,8 @@ export default {
     position: absolute;
     width: 100%;
     height: 100%;
-    background-color:#00afef4b;
+    background-color:#00afef34;
     top: 0;
     
 }
-</style>
+</style> 
